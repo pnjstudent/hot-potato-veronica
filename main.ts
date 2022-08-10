0dcda3e350ab59888fdca5a437ea19611b5a1f95
pnjstudent <110066984+pnjstudent@users.noreply.github.com>
2022-08-10 14:59:41 -0400

    added music and animation

--- main.ts
+++ main.ts
@@ -1,15 +1,14 @@
 let timer = 0
 input.onButtonPressed(Button.A, function () {
     timer = randint(5, 15)
-    basic.showIcon(IconNames.Chessboard)
     music.startMelody(music.builtInMelody(Melodies.Nyan), MelodyOptions.Forever)
     while (timer > 0) {
         timer += -1
-        basic.showIcon(IconNames.SmallHeart)
         basic.showIcon(IconNames.Heart)
+        basic.showIcon(IconNames.SmallHeart)
         basic.pause(1000)
     }
-    music.stopAllSounds()
+    music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
     basic.showIcon(IconNames.Skull)
 })
 basic.forever(function () {
